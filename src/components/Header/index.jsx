776043b74256f31b5440beca3e5008cc61eea029
Juliout <juliocst1993@gmail.com>
2022-10-09 15:08:02 -0300

    1507

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,17 +6,19 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return (
         <HeaderDiv>
+            <Link to='/' style={{color:'white'}}>
             <div className="divlogo">
-                <img src="/image/trevo.png" alt="horoscopologo" />
+             <img src="/image/trevo.png" alt="horoscopologo" />
                 <h1>Alerta da Sorte</h1>
             </div>
+            </Link>
             <div className='logindiv'>
                 <input type="text" name="" id="" autoComplete='none'/>
                 <input type="password" name="" id="" autoComplete='off'/>
                 <Link to='/user/painel'><button>Login</button></Link>
                 <GiPadlock className='iconH'/>
                 <FiEdit className='iconH'/>
-                <Link to='/'><GiHamburgerMenu className='hamburgerMenu'/></Link>
+                <GiHamburgerMenu className='hamburgerMenu'/>
             </div>
 
         </HeaderDiv>
